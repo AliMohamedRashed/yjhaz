0c59b53bbea39ffc34aa31ef652a1db6848de247
AliMohamedMohamedKH <AliMohamedMohamedKH@outlook.com>
2024-08-17 15:31:44 +0300

    Using State Management with Kotlin Flow in Home Feature

--- app/src/main/java/com/ali/advancedtask/feature/home/domin/repository/HomeRepository.kt
+++ app/src/main/java/com/ali/advancedtask/feature/home/domin/repository/HomeRepository.kt
@@ -1,11 +1,16 @@
 package com.ali.advancedtask.feature.home.domin.repository
 
+import com.ali.advancedtask.core.State
+import com.ali.advancedtask.feature.home.data.model.response.HomeDataResponseDto
 import com.ali.advancedtask.feature.home.data.model.response.base_categories.BaseCategoriesResponseDto
 import com.ali.advancedtask.feature.home.data.model.response.popular_sellers.PopularSellersResponseDto
 import com.ali.advancedtask.feature.home.data.model.response.trending_sellers.TrendingSellersResponseDto
+import kotlinx.coroutines.flow.Flow
 
 
 interface HomeRepository {
+
+    suspend fun getCategoriesPopularAndTrendingSellers(): Flow<State<HomeDataResponseDto>>
 
     suspend fun getCategories(): BaseCategoriesResponseDto
 
