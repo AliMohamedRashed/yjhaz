3c9f6e18a787358339ebba5b2747012bbf43a9dc
AliMohamedMohamedKH <AliMohamedMohamedKH@outlook.com>
2024-08-19 20:33:43 +0300

    Using ValidationResult to validate user input before making the apiRequest in the login feature and making the whole app portrait.

---
+++ app/src/main/java/com/ali/advancedtask/feature/login/domin/validation/ValidationResult.kt
@@ -0,0 +1,7 @@
+package com.ali.advancedtask.feature.login.domin.validation
+
+enum class  ValidationResult {
+    SUCCESS,
+    INVALID_EMAIL,
+    EMPTY_PASSWORD,
+}
--- app/src/main/java/com/ali/advancedtask/feature/login/domin/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/ali/advancedtask/feature/login/domin/viewmodel/LoginViewModel.kt
@@ -6,6 +6,7 @@ import com.ali.advancedtask.core.State
 import com.ali.advancedtask.feature.login.data.model.request.LoginRequestDto
 import com.ali.advancedtask.feature.login.data.model.response.LoginResponseDto
 import com.ali.advancedtask.feature.login.domin.repository.LoginRepository
+import com.ali.advancedtask.feature.login.domin.validation.ValidationResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -26,6 +27,18 @@ class LoginViewModel @Inject constructor(
                 _state.value = state
             }
         }
+    }
+
+    fun validateInputs(registerData: LoginRequestDto): ValidationResult {
+        return when {
+            !isEmailValid(registerData.email) -> ValidationResult.INVALID_EMAIL
+            registerData.password.isEmpty() -> ValidationResult.EMPTY_PASSWORD
+            else -> ValidationResult.SUCCESS
+        }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
 }
